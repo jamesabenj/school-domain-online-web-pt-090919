29779e33f3a2b05c1aeb1d2e18e1f37fe73466e0
James Benjamin <james.abenj@gmail.com>
2019-09-30 20:12:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require "pry" 
+class School
+  def initialize(school_name, roster = {})
+    @school_name = school_name
+    @roster = roster
+  end 
+  
+  def school_name
+    @school_name
+  end
+  
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+        if roster.empty?
+      roster[grade] = []
+    roster[grade].collect do |grade_number, student|
+      binding.pry
+      if grade_number == grade
+      roster[grade] << student_name
+      end
+    end
+    end 
+  end
+  
+  def grade(grade_number)
+    roster[grade_number]
+  end 
+  
+  def sort 
+    roster.sort
+  end 
+  
+  
+end 
